8ac609e37b2e394850d6480d39eb20d6b48f69a0
Jimmy <sdosatan915@gmail.com>
2023-09-08 02:36:55 +0800

    加密测试基本通过

---
+++ copy-encrypted-jar-to-config.bat
@@ -0,0 +1,14 @@
+echo "copy and encrypt........."  
+echo.  
+
+java -jar classfinal-fatjar.jar -file D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-agent\target\jeepay-agent.jar -packages com.jeequan.jeepay.agent -exclude com.jeequan.jeepay.agent.bootstrap.JeepayAgentApplication -pwd abcd1234 -Y
+xcopy /y D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-agent\target\jeepay-agent-encrypted.jar D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\conf\agent\jeepay-agent.jar
+
+java -jar classfinal-fatjar.jar -file D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-manager\target\jeepay-manager.jar -packages com.jeequan.jeepay.mgr -exclude com.jeequan.jeepay.mgr.bootstrap.JeepayMgrApplication -pwd abcd1234 -Y
+xcopy /y D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-manager\target\jeepay-manager-encrypted.jar D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\conf\manager\jeepay-manager.jar
+
+java -jar classfinal-fatjar.jar -file D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-merchant\target\jeepay-merchant.jar -packages com.jeequan.jeepay.mch -exclude com.jeequan.jeepay.mch.bootstrap.JeepayMchApplication -pwd abcd1234 -Y
+xcopy /y D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-merchant\target\jeepay-merchant-encrypted.jar D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\conf\merchant\jeepay-merchant.jar
+
+xcopy /y D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-payment\target\jeepay-payment.jar D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\conf\payment
+xcopy /y D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\jeepay-robots\target\jeepay-robots.jar D:\Develop\PayCompany\SourceCode\Aisapay-plus\jeepay-plus\conf\robots
